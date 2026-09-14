310e1a09fae5357104f459c6ee44848ce382c5ad
hthuonq <ldthuong120406@gmail.com>
2026-09-14 20:55:25 +0700

    TUAN01/ LAB01/ Tạo thư mục buổi 2

---
+++ TUAN01/lab1/BUOI2/bai1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BUOI2
+{
+    class bai1
+    {
+        public static void chay()
+        {
+            int a, b;
+            int tong;
+
+            Console.Write("Nhap a, b: ");
+            string[] tam = Console.ReadLine().Split();
+
+            a = int.Parse(tam[0]);
+            b = int.Parse(tam[1]);
+
+            tong = b * (b + 1) / 2 - (a - 1) * a / 2;
+            Console.WriteLine("tong cua cac so trong doan: ", a, b, tong);
+        }
+    }
+}
---
+++ TUAN01/lab1/BUOI2/main.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BUOI2
+{
+    class main
+    {
+        public static void Main(string[] args)
+        {
+            int choice;
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("BUOI 2");
+                Console.WriteLine("1. tinh tong doan");
+                Console.WriteLine("2. luy thua nhanh 1");
+                Console.WriteLine("3. tinh bieu thuc nhanh 1");
+                Console.WriteLine("4. tim qui");
+                Console.WriteLine("0. thoat");
+
+                Console.WriteLine("chon bai: ");
+                choice = int.Parse(Console.ReadLine());
+                Console.Clear();
+
+                switch (choice)
+                {
+                    case 1:
+                        bai1.chay();
+                        break;
+                    case 0:
+                        Console.WriteLine("ket thuc");
+                        break;
+                    default:
+                        Console.WriteLine("lua chon khong hop le");
+                        break;
+                }
+                if (choice != 0)
+                {
+                    Console.WriteLine();
+                    Console.WriteLine("nhan Enter de quay lai menu");
+                    Console.ReadLine();
+
+                }
+
+            } while (choice != 0);
+        }
+    }
+}
